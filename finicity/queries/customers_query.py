d12c4ba167330afb466e5741f23c0a1921950486
Jeremy <jeremy.lakey@gmail.com>
2020-02-29 16:20:01 -0700

    code cleaning

--- finicity/queries/customers_query.py
+++ finicity/queries/customers_query.py
@@ -11,24 +11,21 @@ class CustomersQuery(object):
     def batches(self, batch_size: int = 25):
         i = 1
         while 1:
-            batch = self._get_customers(search_term=self.__search_term, start=i, limit=batch_size)
-            for institution in batch.customers:
-                yield institution
+            batch = self.__fetch(start=i, limit=batch_size)
+            yield batch.customers
             i += batch_size
             if not batch.moreAvailable:
                 break
 
     # https://community.finicity.com/s/article/Get-Customers
     # GET /aggregation/v1/customers?search=[text]&start=[index]&limit=[count]&type=[type]&username=[username]
-    def _get_customers(self, search_term: str = "*", username: str = None, start: int = 1, limit: int = 25) -> CustomersListResponse:
+    def __fetch(self, start: int = 1, limit: int = 25) -> CustomersListResponse:
         """
         Find all customers enrolled by the current partner, where the search text is found in the customer's username or any combination of firstName and lastName fields. If no search text is provided, return all customers.
         Valid values for type are testing, active.
         If the value of moreAvailable in the response is true, you can retrieve the next page of results by increasing the value of the start parameter in your next request:
         ...&start=6&limit=5
 
-        :param search_term: The text you wish to match. Leave this empty if you wish to return all customers.
-        :param username: Username for exact match. (Will return 0 or 1 records.)
         :param start: Starting index for this page of results. The default value is 1.
         :param limit: Maximum number of entries for this page of results. The default value is 25.
         :return:
@@ -36,6 +33,8 @@ class CustomersQuery(object):
         # note ripped off search_term: Must be URL-encoded (see Handling Spaces in Queries)
         # also do type = testing / active / [blank for all]
         # self._get_with_token()
+        search_term = self.__search_term
+        username = self.__username
         path = "/aggregation/v1/customers"
         params = {
             "start": start,
--- finicity/queries/institutions_query.py
+++ finicity/queries/institutions_query.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional
 
 from finicity.api_http_client import ApiHttpClient
 from finicity.models import InstitutionsListResponse
@@ -12,21 +12,19 @@ class InstitutionsQuery(object):
     def batches(self, batch_size: int = 25):
         i = 1
         while 1:
-            batch = self._get_institutions(search_term=self.__search_term, start=i, limit=batch_size)
-            for institution in batch.institutions:
-                yield institution
+            batch = self.__fetch(start=i, limit=batch_size)
+            yield batch.institutions
             i += batch_size
             if not batch.moreAvailable:
                 break
 
-    def _get_institutions(self, search_term: str = "*", start: int = 1, limit: int = 25) -> InstitutionsListResponse:
+    def __fetch(self, start: int = 1, limit: int = 25) -> InstitutionsListResponse:
         """Use this call to search all Financial Institutions (FI) the Finicity has connections with and supports.
         Return all financial institutions that contain the search text in the institution’s name, urlHomeApp, or urlLogonApp fields.
         To get a list of all FI’s, leave the search parameter out of the call.  If the search query is left blank, the API will return an error.
         If the value of moreAvailable in the response is true, you can retrieve the next page of results by increasing the value of the start parameter in your next request:
           ...&start=6&limit=5
 
-        :param search_term: Text to match, or omit the search parameter.
         :param start: Starting index for this page of results
         :param limit: Maximum number of entries for this page of results
         :return:
@@ -37,8 +35,8 @@ class InstitutionsQuery(object):
             "start": start,
             "limit": limit,
         }
-        if search_term and search_term != "*":
-            params["search"] = search_term
+        if self.__search_term and self.__search_term != "*":
+            params["search"] = self.__search_term
         response = self.__http_client.get(path, params=params)
         response_dict = response.json()
         return InstitutionsListResponse.from_dict(response_dict)
