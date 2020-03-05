85f3191f95f0064d7a4567adf4a8af03d2d92c56
Jeremy <jeremy.lakey@gmail.com>
2020-03-04 17:06:23 -0700

    rough draft of aggregation link generation

--- finicity/resources/connections.py
+++ finicity/resources/connections.py
@@ -41,3 +41,33 @@ class Connections(object):
         response = self.__http_client.post(path, data)
         response_dict = response.json()
         return GenerateLinkResponse.from_dict(response_dict).link
+
+    # https://community.finicity.com/s/article/Generate-Finicity-Connect-URL
+    def generate_aggregation_link(self,
+                          customer_id: str,
+                          consumer_id: str,
+                          content_type: ContentType = ContentType.JSON,
+                          webhook: Optional[str] = None,
+                          webhook_data: Optional[Mapping[str, str]] = None,
+                          analytics: Optional[str] = None,
+                          ) -> str:
+        data = {
+            'partnerId': self.__partner_id,
+            'customerId': customer_id,
+            'consumerId': consumer_id,
+            'redirectUrl': consumer_id,
+            'type': ConnectType.aggregation.value,
+            'ContentType': content_type.value,
+        }
+        if webhook:
+            data['webhook'] = webhook
+        if webhook_data:
+            data['webhookData'] = webhook_data
+        if analytics:
+            data['analytics'] = analytics
+        path = f"/connect/v1/generate"
+        response = self.__http_client.post(path, data)
+        response_dict = response.json()
+        return GenerateLinkResponse.from_dict(response_dict).link
+
+    # TODO note: from_date only for VOA
